c912b5f3f9ce5edd58fd3bd48957b6965082378e
Small Coding <smallcoding93@gmail.com>
2021-05-01 00:50:56 +0200

    JS added that creates and adds Xs and Os

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 let gameboard = [];
 
+const symbolModule = (() => {
+    let currentTurn = 1;
+
+    function getSymbol(boxNum) {
+        if(currentTurn % 2 == 1) {
+            gameboard[boxNum].symbol = player1.symbol;
+            gameboard[boxNum].symbol = player1.symbol;
+        }
+        else {
+            gameboard[boxNum].symbol = player2.symbol;
+            gameboard[boxNum].symbol = player2.symbol;
+        }
+        currentTurn += 1;
+    }
+
+    function addSymbol(boxNum) {
+        const symbolContainer = document.createElement("div");
+        symbolContainer.classList.add("symbol");
+        symbolContainer.classList.add("symbol" + gameboard[boxNum].symbol);
+        gameboard[boxNum].box.appendChild(symbolContainer);
+        const symbol = document.createTextNode(gameboard[boxNum].symbol);
+        symbolContainer.appendChild(symbol);
+    }
+
+    return {
+        getSymbol: getSymbol,
+        addSymbol: addSymbol
+    };
+})();
+
 const playerFactory = (playerNum, playerSymbol) => {
     const name = "Player " + playerNum;
     const symbol = playerSymbol;
@@ -14,12 +44,17 @@ const boxFactory = (board, boxNum) => {
     let symbol = boxNum;
 
     box.addEventListener("click", () => {
-        if(symbol == boxNum) {
-            
+        if(gameboard[boxNum].symbol == boxNum) {
+            createSymbol(boxNum);
         }
     })
 
     return {symbol, box};
+}
+
+function createSymbol(boxNum) {
+    symbolModule.getSymbol(boxNum);
+    symbolModule.addSymbol(boxNum);
 }
 
 function createPlayers() {
@@ -50,7 +85,6 @@ function initialize() {
 
 let player1;
 let player2;
-let currentTurn = 1;
 
 initialize();
 
